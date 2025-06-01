f3c89c8f678efd4615cd1104fa62ae6fcb2e6d37
yamaimo-102 <myice.f01@gmail.com>
2025-06-01 15:42:29 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP連想配列課題</title>
+</head>
+
+<body>
+  <p>
+    <pre>
+    <?php
+    $vegetable = ['name' => 'onion', 'price'=> 200, 'weight'=> 160];
+
+    // 連想配列の値を出力する
+    print_r($vegetable);
+    ?>
+    </pre>
+  </p>
+</body>
+
+</html>
